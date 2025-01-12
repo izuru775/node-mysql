d670b85d35571e13ad97d20d4202ebf2fca16333
Isuru Adikaram <izuru775@gmail.com>
2025-01-12 11:37:14 +1100

    connection details are now coming from .env

--- dbConnect.js
+++ dbConnect.js
@@ -1,10 +1,11 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 const sequelize = new Sequelize({
-    database: 'mydb',
-    username: 'root',
-    password: 'toor',
-    host: 'localhost',
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
 });
 
